e676f8acb5b38ae2b4e502a9263e36af184b9269
Kenneth Simpson <kenneth.mathis.simpson@gmail.com>
2025-01-03 14:06:11 -0600

    updated axios file to display env var

--- client/src/data/services/axiosService.js
+++ client/src/data/services/axiosService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_EXPRESS_SERVER;
+console.log(process.env.REACT_APP_EXPRESS_SERVER);
 
 const axiosService = {
   get: axios.get,
